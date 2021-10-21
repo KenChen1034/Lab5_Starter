c604b5f9daa7b730580a47c30fea91715ebc1f16
KenChen1034 <60757144+KenChen1034@users.noreply.github.com>
2021-10-20 22:55:23 -0700

    explore_expose_done

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,62 @@
 // explore.js
-
+var synth = window.speechSynthesis;
 window.addEventListener('DOMContentLoaded', init);
 
+
+
 function init() {
-  // TODO
+  var button = document.querySelector('button');
+  var textToSpeak = document.querySelector('textarea');
+  var img = document.querySelector('img');
+  var voices = [];
+  var voiceSelect = document.querySelector('select');
+  
+  
+  if(speechSynthesis !== undefined){
+    speechSynthesis.onvoiceschanged = GETVOICES;
+  }
+  
+  function GETVOICES(){
+    voices = synth.getVoices();
+  
+    for(var i =0; i< voices.length; i++){
+      var option = document.createElement('option');
+      option.textContent = voices[i].name + '(' + voices[i].lang + ')';
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+  }
+    
+  
+    
+  textToSpeak.addEventListener('input', event =>{
+      textToSpeak.placeholder = event.target.value;
+  });
+    
+  button.addEventListener('click', event =>{
+    var utterance = new SpeechSynthesisUtterance(textToSpeak.placeholder);
+    var selectedVoiceName = voiceSelect.selectedOptions[0].getAttribute('data-name');
+    for(let i = 0; i< voices.length;i++){
+      if(voices[i].name === selectedVoiceName){
+        utterance.voice = voices[i];
+      }
+    }
+    
+    synth.speak(utterance);
+  });
+  setInterval(checkSpeak,100);
+  function checkSpeak(){
+    if(synth.speaking){
+      img.setAttribute("src", "assets/images/smiling-open.png");
+    }
+    else{
+      img.setAttribute("src", "assets/images/smiling.png");
+    }
+  }
 }
+
+
+
+
+
